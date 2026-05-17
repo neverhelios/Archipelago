c1c7b815df4be033daf297e9f8087b86355fd067
neverhelios <elias.bina58@gmail.com>
2026-05-17 20:31:24 +0200

    Deathbulge: Add all hardlock rules
    Well, there's all the rules, the only thing that can make you bk are the Bus Floor tickets
    I'm gonna set the bosses as forced checks
    because they block some progression
    but i don't think this will really change something

--- worlds/deathbulge/rules.py
+++ worlds/deathbulge/rules.py
@@ -1,21 +1,113 @@
-from . import DeathbulgeWorld
+from typing import Dict, Callable, TYPE_CHECKING
+from .items import ItemDict, treasure_legendary_beats_items
 
-from typing import Dict, Callable
-from BaseClasses import CollectionState
+if TYPE_CHECKING:
+    from . import DeathbulgeWorld
+else:
+    DeathbulgeWorld = object
+
+from BaseClasses import CollectionState, CollectionRule
 
 
 class DeathbulgeRules:
     player: int
     world: DeathbulgeWorld
+    connection_rules: dict[str, CollectionRule]
     location_rules: Dict[str, Callable[[CollectionState], bool]]
     region_rules: Dict[str, Callable[[CollectionState], bool]]
 
-    # def __init__(self, world: DeathbulgeWorld) -> None:
-    #     self.player = world.player
-    #     self.world = world
+    def __init__(self, world: DeathbulgeWorld) -> None:
+        self.player = world.player
+        self.world = world
 
-    #     self.region_rules = {
-    #         "Act 2": self.has_act2_requirements,
-    #         "Act 3": self.has_act3_requirements,
-    #         "Epilogue": self.has_epilogue_requirements,
-    #     }
+        self.connection_rules = {
+            "TheBus - TheBusElevator -> TheBus - TheBus07": self.has_13_deck_keycard,
+            "TheBus - TheBusElevator -> TheBus - TheBus08": self.has_14_deck_keycard,
+            "TheBus - TheBusElevator -> TheBus - TheBus10": self.has_16_deck_keycard,
+            "Hoho - Hoho01Lower -> BattleOfTheBands - BOTBLobby": self.has_all_legendary_beats,
+            # Connections with indirect conditions
+            "Bopstead - Bopstead02 -> Basement - Basement01": self.has_beaten_kkwak_claire,
+            "Bopstead - Bopstead02 -> TheBus - TheBus01": self.has_beaten_modern_babby,
+            "TheBus - TheBus10 -> Hoho - Hoho01-Bus": self.has_beaten_platinum_scrumptious,  # Might be trivially true ?
+            "Lab - Lab10 -> Hoho - Hoho02": self.has_beaten_mutilla,  # Might be trivially true ?
+            "Hoho - Hoho02 -> Lab - Lab10": self.has_beaten_mutilla,
+            "Hoho - Hoho02 -> Pokalyps - Pokalyps01": self.has_beaten_mutilla,
+            "ClaireHair - ClaireHair04Lower -> ClaireLower - ClaireLower01": self.has_beaten_pokalyps,
+        }
+
+        self.location_rules = {
+            "Hoho02Treasure06": self.has_all_legendary_beats,
+        }
+
+        # dict of connection names and the regions checked in the requirements to traverse the exit
+        self.indirect_conditions = {
+            "Bopstead - Bopstead02 -> Basement - Basement01": [
+                self.world.get_region("ClaireHair - ClaireHair08"),
+            ],
+            "Bopstead - Bopstead02 -> TheBus - TheBus01": [
+                self.world.get_region("Basement - Basement07"),
+            ],
+            "TheBus - TheBus10 -> Hoho - Hoho01-Bus": [
+                self.world.get_region("TheBus - TheBus10"),
+            ],
+            "Lab - Lab10 -> Hoho - Hoho02": [
+                self.world.get_region("Lab - Lab10"),
+            ],
+            "Hoho - Hoho02 -> Lab - Lab10": [
+                self.world.get_region("Lab - Lab10"),
+            ],
+            "Hoho - Hoho02 -> Pokalyps - Pokalyps01": [
+                self.world.get_region("Lab - Lab10"),
+            ],
+            "ClaireHair04Lower -> ClaireLower - ClaireLower01": [
+                self.world.get_region("Pokalyps - Pokalyps11"),
+            ],
+        }
+
+    # Items rules
+    def has_13_deck_keycard(self, state: CollectionState) -> bool:
+        return state.has("[Key Merch] 13th Deck Keycard", self.player)
+
+    def has_14_deck_keycard(self, state: CollectionState) -> bool:
+        return state.has("[Key Merch] 14th Deck Keycard", self.player)
+
+    def has_16_deck_keycard(self, state: CollectionState) -> bool:
+        return state.has("[Key Merch] 16th Deck Keycard", self.player)
+
+    def has_all_legendary_beats(self, state: CollectionState) -> bool:
+        for legendary_beat in treasure_legendary_beats_items:
+            if not state.has(legendary_beat["name"], self.player):
+                return False
+        return True
+
+    # Bosses rules
+    # TODO: Create location checks for the bosses to really enforce the fact that they?
+    def has_beaten_kkwak_claire(self, state: CollectionState) -> bool:
+        return state.can_reach_region("ClaireHair - ClaireHair08", self.player)
+
+    def has_beaten_modern_babby(self, state: CollectionState) -> bool:
+        return state.can_reach_region("Basement - Basement07", self.player)
+
+    def has_beaten_platinum_scrumptious(self, state: CollectionState) -> bool:
+        return state.can_reach_region("TheBus - TheBus10", self.player)
+
+    def has_beaten_mutilla(self, state: CollectionState) -> bool:
+        return state.can_reach_region("Lab - Lab10", self.player)
+
+    def has_beaten_pokalyps(self, state: CollectionState) -> bool:
+        return state.can_reach_region("Pokalyps - Pokalyps11", self.player)
+
+    # Set all rules in the multiworld
+    def set_deathbulge_rules(self) -> None:
+        multiworld = self.world.multiworld
+
+        for entrance_name, rule in self.connection_rules.items():
+            entrance = multiworld.get_entrance(entrance_name, self.player)
+            entrance.access_rule = rule
+
+            for region in self.indirect_conditions.get(entrance_name, ()):
+                multiworld.register_indirect_condition(region, entrance)
+
+        for loc in multiworld.get_locations(self.player):
+            if loc.name in self.location_rules:
+                loc.access_rule = self.location_rules[loc.name]
